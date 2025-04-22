f188ce6e2eeb61e569a42dc1304061fb78e505be
antuneseds <esterdossantosantunes04@gmail.com>
2025-04-22 16:50:26 -0300

    feat: Criação da API Modelo

---
+++ core/models/modelo.py
@@ -0,0 +1,7 @@
+class Modelo(models.Model):
+    nome = models.CharField(max_length=80)
+    marca = models.CharField(max_length=80, null=True, blank=True)
+    categoria = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return f"({self.id}) {self.nome} ({self.marca})"
